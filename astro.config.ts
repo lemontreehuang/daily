90471c46821dcbd51a4a2a9cb7924a4d5ea136a2
Your Name <you@example.com>
2026-03-10 14:43:13 +0800

    dd

--- astro.config.ts
+++ astro.config.ts
@@ -25,11 +25,11 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://astro-pure.js.org',
+  site: 'https://lemontreehuang.github.io',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
-  // base: '/astro-pure/',
-  trailingSlash: 'never',
+  base: '/blog',
+  trailingSlash: 'always',
   // root: './my-project-directory',
   server: { host: true },
 
--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: 'Tick Tick Boom',
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'LemonTree',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: 'Help People. Shape AI. Keep Growing.',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
